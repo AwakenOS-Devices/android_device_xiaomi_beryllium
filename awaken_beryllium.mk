f8bdc0450445f74287a092679de234d94bf52ce9
PainKiller3 <ninadpatil100@gmail.com>
2021-05-13 16:44:07 +0530

    beryllium: Awaken-ify
    Change-Id: Ic4dc2162c1e08aedca817c291ea20fc6c09dffd3
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- awaken_beryllium.mk
+++ awaken_beryllium.mk
@@ -6,11 +6,15 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Awaken stuff.
+$(call inherit-product, vendor/awaken/config/common.mk)
+
+# Awaken flags
+AWAKEN_BUILD_TYPE := OFFICIAL
+IS_PHONE:=true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := awaken_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
